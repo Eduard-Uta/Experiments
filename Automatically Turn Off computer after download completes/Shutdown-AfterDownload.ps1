c9efb005ea9f42ab5e2f4ac08c002937a7b255c5
Eduard Uta <eduard.uta@gmail.com>
2022-01-16 01:13:37 +0200

    Initial version.

---
+++ Automatically Turn Off computer after download completes/Shutdown-AfterDownload.ps1
@@ -0,0 +1,21 @@
+﻿$downloadDir = "C:\Users\edi\Downloads"
+
+while (1) 
+{
+    (Get-ChildItem -Path $downloadDir).Refresh()
+    $s1 = Get-ChildItem -Path $downloadDir | Measure-Object -Property Length -Sum | Select-Object Sum
+    write-host $s1.Sum
+
+    Start-Sleep -s 5
+
+    (Get-ChildItem -Path $downloadDir).Refresh()
+    $s2 = Get-ChildItem -Path $downloadDir | Measure-Object -Property Length -Sum | Select-Object Sum
+    write-host $s2.Sum
+
+    if ($s1.Sum -eq $s2.sum) 
+    {
+        write-host "Download completed, shutting down.."
+        shutdown /s
+    }
+
+}
