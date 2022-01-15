a30e4159ca3916f2cb2c197fcbf6f827f0684109
Eduard Uta <eduard.uta@gmail.com>
2022-01-16 00:28:24 +0200

    Initial version.

---
+++ Automatically save work files using Powershell/Save-Work.ps1
@@ -0,0 +1,8 @@
+﻿# source C:\ws2\Powershell
+# target E:\backup
+
+$currentDate = Get-Date -Format 'yyyy-MM-dd'
+
+Get-ChildItem -Path "C:\ws2\Powershell" | `
+    Where-Object {$_.LastWriteTime -ge (Get-Date).AddDays(-1)} | `
+    Compress-Archive -DestinationPath "E:\backup\Powershell_ws_$currentDate.zip" -Update
